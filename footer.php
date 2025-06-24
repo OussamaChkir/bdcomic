9097b73bc184c3adb07f6f8f9d50c72ec36d3089
Amal_Gadhrib <amal.gadhrib@softtodo.com>
2025-06-24 16:59:47 +0100

    KORSCH-18 Page- / Post- / Custom Post-Settings

--- footer.php
+++ footer.php
@@ -1,3 +1,61 @@
+<!------------------------ Prefooter ------------------------>
+<?php 
+    $prefooter = get_field('prefooter');
+    $hide_prefooter = $prefooter['hide_prefooter'];
+
+    if (!$hide_prefooter) :
+        wp_enqueue_style('prefooter', get_template_directory_uri() . '/assets/css/Partials/prefooter.css', array(), '1.0', 'all');
+
+        $prefooter_image = $prefooter['prefooter_image'];
+
+        $header = get_field('header', 'option');
+        $subheader = get_field('subheader', 'option');
+        $text = get_field('text', 'option');
+        $contact_form_link = get_field('contact_form_link', 'option');
+    ?>
+
+        <div class="prefooter">
+            <div class="container">
+                <?php if ($header || $subheader || $text || $second_text): ?>
+                    <div class="header-wrapper">
+                        <?php if ($header): ?>
+                            <div class="header">
+                                <<?php echo $header_type; ?> class="h1"><?php echo esc_html($header); ?></<?php echo $header_type; ?>>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php if ($subheader): ?>
+                            <div class="subheader h3">
+                                <?php echo esc_html($subheader); ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php if ($text && $second_text): ?>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="text text-medium">
+                                        <?php echo $text; ?>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="second_text text-medium">
+                                        <?php echo $second_text; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php elseif ($text): ?>
+                            <div class="text text-medium">
+                                <?php echo $text; ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    <?php endif;
+?>
+
+<!------------------------ Footer ------------------------>
 <?php 
     $footer_logo = get_field('footer_logo', 'option');
     $addresses = get_field('addresses', 'option');

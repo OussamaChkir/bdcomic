d16cdf9b4dca09a70f168ef518719c7f24f9561f
Amal_Gadhrib <amal.gadhrib@softtodo.com>
2025-06-30 09:43:46 +0100

    CE TExt

--- components/block-teaser-list-v1.php
+++ components/block-teaser-list-v1.php
@@ -20,11 +20,7 @@ if ($teaser_list_v1) :
                     <div class="header-wrapper">
                         <?php if ($header): ?>
                             <div class="header">
-                                <?php if ( is_front_page() ) : ?>
-                                    <<?php echo $header_type; ?> class="h1"><?php echo esc_html($header); ?></<?php echo $header_type; ?>>
-                                <?php else : ?>
-                                    <<?php echo $header_type; ?>><?php echo esc_html($header); ?></<?php echo $header_type; ?>>
-                                <?php endif; ?>
+                                <h2 class="<?php echo $header_type; ?>"><?php echo esc_html($header); ?></h2>
                             </div>
                         <?php endif; ?>
 
--- components/block-text-bild-teaser-mit-button.php
+++ components/block-text-bild-teaser-mit-button.php
@@ -18,11 +18,7 @@ if ($text_bild_teaser_mit_button) :
             <div class="block-container">
                 <?php if ($header): ?>
                     <div class="header">
-                        <?php if ( is_front_page() ) : ?>
-                            <<?php echo $header_type; ?> class="h1"><?php echo esc_html($header); ?></<?php echo $header_type; ?>>
-                        <?php else : ?>
-                            <<?php echo $header_type; ?>><?php echo esc_html($header); ?></<?php echo $header_type; ?>>
-                        <?php endif; ?>
+                        <h2 class="<?php echo $header_type; ?>"><?php echo esc_html($header); ?></h2>
                     </div>
                 <?php endif; ?>
 

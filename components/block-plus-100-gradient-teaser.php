c3bff265702ee04136c6fa013066ee73cd1a6cbc
Amal_Gadhrib <amal.gadhrib@softtodo.com>
2025-07-10 15:29:49 +0100

    KORSCH-16 CE +100 Gradient Teaser

--- components/block-plus-100-gradient-teaser.php
+++ components/block-plus-100-gradient-teaser.php
@@ -4,12 +4,14 @@ wp_enqueue_style('block-100-gradient-teaser', get_template_directory_uri() . '/a
 $gradient_teaser = get_field('100_gradient_teaser');
 
 if ($gradient_teaser) :
+    $header = $gradient_teaser['header'];
+    $subheader = $gradient_teaser['subheader'];
     $text = $gradient_teaser['text'];
     $image = $gradient_teaser['image'];
     $link = $gradient_teaser['link'];
 ?>
 
-    <div class="block-100-gradient-teaser">
+    <div class="block-100-gradient-teaser <?php if ($subheader && $text): ?>lessSpace<?php endif; ?>">
         <div class="container">
             <div class="row">
                 <div class="col-xl-6">
@@ -22,10 +24,22 @@ if ($gradient_teaser) :
                         </div>
                     <?php endif; ?>
                 </div>
-                <div class="col-xl-6">
-                    <?php if ($text): ?>
-                        <h2 class="text text-white m-0"><?php echo $text; ?></h2>
-                    <?php endif; ?>
+                <div class="col-xl-6 p-xl-0">
+                    <div class="header-wrapper">
+                        <?php if ($header): ?>
+                            <h2 class="header text-white m-0"><?php echo esc_html($header); ?></h2>
+                        <?php endif; ?>
+
+                        <?php if ($subheader): ?>
+                            <div class="subheader h3 text-white">
+                                <?php echo esc_html($subheader); ?>
+                            </div>
+                        <?php endif; ?>
+
+                        <?php if ($text): ?>
+                            <div class="text text-medium text-white m-0"><?php echo $text; ?></div>
+                        <?php endif; ?>
+                    </div>
 
                     <?php if ($link): ?>
                         <a class="icon-arrow-right-white float-end" href="<?php echo $link["url"]; ?>" target="<?php echo $link["target"]; ?>"><span class="d-none"><?php echo $link["title"]; ?></span></a>
